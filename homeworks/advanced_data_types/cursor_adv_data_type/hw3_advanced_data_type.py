cfe9a7085a5b06426b1c35fcf72ce3396605d738
Andrii Yanchenko <andrey.ya2006@gmail.com>
2022-07-10 02:42:21 +0300

    ready

--- homeworks/advanced_data_types/cursor_adv_data_type/hw3_advanced_data_type.py
+++ homeworks/advanced_data_types/cursor_adv_data_type/hw3_advanced_data_type.py
@@ -1,8 +1,5 @@
-from apt import print_function
-
-
+#1. Define the id of next variables:
 int_a = 55
-print(id(int_a))
 print('This is id of int_a ' + str(id(int_a)))
 str_b = 'cursor'
 print('This is id of str_b ' + str(id(str_b)))
@@ -18,9 +15,9 @@ print(lst_d)
 print('This is id lst_d with new element ' + str(id(lst_d)))
 lst_d.pop()
 for var in lst_d_new_element:
-    lst_d.append(var)
-    print(id(lst_d))
-    print(lst_d)
+	lst_d.append(var)
+	print(id(lst_d))
+	print(lst_d)
 print(type(int_a))
 print(type(str_b))
 print(type(set_c))
@@ -39,75 +36,83 @@ print('Anna has {appl} apples and {peach} peaches.'.format(appl=apples, peach=pe
 print('Anna has {appl:5} apples and {peach:3} peaches.'.format(appl=apples, peach=peaches))
 print(f'Anna has {apples} apples and {peaches} peaches.')
 print('Anna has %d apples and %d peaches.' % (apples, peaches))
-dict_fruit = {'fruit1': 5, 'fruit2' : 7}
+dict_fruit = {'fruit1': 5, 'fruit2': 7}
 print('Anna has {fruit1} apples and {fruit2} peaches.'.format(**dict_fruit))
 print(f'Anna has {dict_fruit["fruit1"]} apples and {dict_fruit["fruit2"]} peaches.')
 lst = []
 for num in range(10):
-    if num % 2 == 1:
-        lst.append(num ** 2)
-    else:
-        lst.append(num ** 4)
+	if num % 2 == 1:
+		lst.append(num ** 2)
+	else:
+		lst.append(num ** 4)
 print(lst)
-lst_to_lst_comp = [ num ** 2 if num % 2 == 1 else num ** 4 for num in range(10)]
+lst_to_lst_comp = [num ** 2 if num % 2 == 1 else num ** 4 for num in range(10)]
 print(lst_to_lst_comp)
 list_comprehension = [num // 2 if num % 2 == 0 else num * 10 for num in range(10)]
 print(list_comprehension)
 list_comprehension_to_regular = []
 for x in range(10):
-    if x % 2 == 0:
-        list_comprehension_to_regular.append(x // 2)
-    else:
-        list_comprehension_to_regular.append(x * 10)
+	if x % 2 == 0:
+		list_comprehension_to_regular.append(x // 2)
+	else:
+		list_comprehension_to_regular.append(x * 10)
 print(list_comprehension_to_regular)
 d = {}
 for num in range(1, 11):
-    if num % 2 == 1:
-        d[num] = num ** 2
+	if num % 2 == 1:
+		d[num] = num ** 2
 print(d)
 
-dict_compr = {x : x ** 2 for x in range(1, 11) if x % 2 == 1 }
+dict_compr = {x: x ** 2 for x in range(1, 11) if x % 2 == 1}
 print(dict_compr)
 d = {}
 for num in range(1, 11):
-    if num % 2 == 1:
-        d[num] = num ** 2
-    else:
-        d[num] = num // 0.5
+	if num % 2 == 1:
+		d[num] = num ** 2
+	else:
+		d[num] = num // 0.5
 print(d)
-dict_compr = {x : (x **2 if x % 2 == 1 else x // 0.5) for x in range(1, 11)}
+dict_compr = {x: (x ** 2 if x % 2 == 1 else x // 0.5) for x in range(1, 11)}
 print(dict_compr)
-dict_comprehension = {x: x**3 for x in range(10) if x**3 % 4 == 0}
+dict_comprehension = {x: x ** 3 for x in range(10) if x ** 3 % 4 == 0}
 print(dict_comprehension)
 dict_compr_regular = {}
 for x in range(10):
-    if x**3 % 4 == 0:
-        dict_compr_regular[x] = x ** 3
+	if x ** 3 % 4 == 0:
+		dict_compr_regular[x] = x ** 3
 print(dict_compr_regular)
-dict_comprehension = {x: x**3 if x**3 % 4 == 0 else x for x in range(10)}
+dict_comprehension = {x: x ** 3 if x ** 3 % 4 == 0 else x for x in range(10)}
 print(dict_comprehension)
 dict_compr_regular = {}
 for x in range(10):
-    if x ** 3 % 4 == 0:
-        dict_compr_regular[x] = x ** 3
-    else:
-        dict_compr_regular[x] = x
+	if x ** 3 % 4 == 0:
+		dict_compr_regular[x] = x ** 3
+	else:
+		dict_compr_regular[x] = x
 print(dict_compr_regular)
+
+
 def foo(x, y):
-    if x < y:
-        return x
-    else:
-        return y
+	if x < y:
+		return x
+	else:
+		return y
+
+
 print(foo(5, 6))
-my_lam = lambda x, y: x if x < y  else y
+my_lam = lambda x, y: x if x < y else y
 print(my_lam(5, 6))
 foo = lambda x, y, z: z if y < x and x > z else y
-print(foo(4, 7 , 12))
+print(foo(4, 7, 12))
+
+
 def lambda_to_foo(x, y, z):
-    if y < x and x > z:
-        return z
-    else:
-        return y
+	if y < x and x > z:
+		return z
+	else:
+		return y
+
+
 print(lambda_to_foo(4, 7, 12))
 lst_to_sort = [5, 18, 1, 24, 33, 15, 13, 55]
 print(sorted(lst_to_sort))
@@ -117,9 +122,20 @@ list_A = [2, 3, 4]
 list_B = [5, 6, 7]
 new_lst = map(lambda x, y: x + y, list_A, list_B)
 print(list(new_lst))
-new_lst_to_sort = filter(lambda x: x % 2 == 1, lst_to_sort)
+new_lst_to_sort = filter(lambda element: element % 2 == 1, lst_to_sort)
 print(list(new_lst_to_sort))
-lst_negatave_munbers = filter(lambda x: x < 0, range(-10, 10))
-print(list(lst_negatave_munbers))
-list_1 = [1,2,3,5,7,9]
-list_2 = [2,3,5,6,7,8]
+lst_neg_number = filter(lambda element: element < 0, range(-10, 10))
+print(list(lst_neg_number))
+list_1 = [1, 2, 3, 5, 7, 9]
+list_2 = [2, 3, 5, 6, 7, 8]
+
+def compare_list(lst1, lst2):
+	def compare_elemets(v):
+		if (v in lst1):
+			return True
+		else:
+			return False
+	filtered = filter(compare_elemets, lst2)
+	print(f'The filtered letters are:{list(filtered)}')
+
+compare_list(list_1, list_2)
